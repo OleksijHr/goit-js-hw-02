e60ac884bac9d68aff764b02751e2b9f693ba355
OleksijHr <hrymovskij@gmail.com>
2024-02-02 16:09:25 +0200

    add changes

--- js/task-4.js
+++ js/task-4.js
@@ -1,17 +1,16 @@
 function getShippingCost(country) {
-    let price;
     switch (country) {
         case "China":
-            price = 100;
+           price = 100;
             return `Shipping to ${country} will cost ${price} credits`;
-        case "Chile": 
-            price = 250;
+        case "Chile":
+           price = 250;
             return `Shipping to ${country} will cost ${price} credits`;
-        case "Australia":
-            price = 170;
+        case "Australia":  
+           price = 170;
             return `Shipping to ${country} will cost ${price} credits`;
         case "Jamaica": 
-            price = 120;
+           price = 120;
             return `Shipping to ${country} will cost ${price} credits`;
         default:
             return "Sorry, there is no delivery to your country";
